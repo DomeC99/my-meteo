87191bd8de6c76172dd9c12e0eda817f71308ff8
DomeC99 <domee9910@gmail.com>
2025-09-01 17:32:57 -0500

    Added icon and date functionality

--- src/script.js
+++ src/script.js
@@ -11,7 +11,29 @@ function updateData (response){ //13. Shows JASON or OBJECT with info. Function 
     let newWind = document.querySelector("#wind");
     newWind.innerHTML = response.data.wind.speed;
 
+    let icon = `<img src= "${response.data.condition.icon_url}" class="weather-icon">`; //27. Defining new HTML 
+    let newIcon = document.querySelector(".icon"); //28. Selects the class in the HTML
+    newIcon.innerHTML = icon; //29. Rewrites the HTML
+
+    let newDate = document.querySelector("#time") //18. Selects the time ID
+    let date = new Date(response.data.time * 1000); //19. Creates new date, to access the API time -> x1000
+    newDate.innerHTML = formatDate(date); //20. replaces day and time (hour and mins)
 }
+
+function formatDate (date){ //21. function to format the date
+    let day = date.getDay(); //22. variables to get the info
+    let hour = date.getHours();
+    let minutes = date.getMinutes();
+
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]; //23. array to replace the day instead of numbers
+    day = days[date.getDay()]; //24. update day variable
+
+    if (minutes < 10) { //26. conditon for < 10 minutes
+        minutes = `0${minutes}`;
+    }
+    return `${day} ${hour}:${minutes}`; //25. result
+}
+
 function searchCityApi (city){//8. Function (2) for intertwining the value with the API cities *city is new name for paramenter inputCity.value
     let apiKey = "28fac95a24b3ba61410a05dt43ob3b30"; //10. define KEY
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`; //11. replace with KEY and CITY (new parameter)
