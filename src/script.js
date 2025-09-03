941e9521c82a0b753fefddf550b928d220377db7
DomeC99 <domee9910@gmail.com>
2025-09-03 10:25:18 -0500

    API integration

--- src/script.js
+++ src/script.js
@@ -18,6 +18,8 @@ function updateData (response){ //13. Shows JASON or OBJECT with info. Function 
     let newDate = document.querySelector("#time") //18. Selects the time ID
     let date = new Date(response.data.time * 1000); //19. Creates new date, to access the API time -> x1000
     newDate.innerHTML = formatDate(date); //20. replaces day and time (hour and mins)
+
+    getForeData(response.data.city);
 }
 
 function formatDate (date){ //21. function to format the date
@@ -36,7 +38,7 @@ function formatDate (date){ //21. function to format the date
 
 function searchCityApi (city){//8. Function (2) for intertwining the value with the API cities *city is new name for paramenter inputCity.value
     let apiKey = "28fac95a24b3ba61410a05dt43ob3b30"; //10. define KEY
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`; //11. replace with KEY and CITY (new parameter)
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`; //11. replace with KEY and CITY (new parameter)
     axios.get(apiUrl).then(updateData); //12. access to axios, then get API, then new function to update info (3)
 }
 function showCity (event){ //3. define function (1). Sends city input for the API in next function.
@@ -47,23 +49,38 @@ function showCity (event){ //3. define function (1). Sends city input for the AP
     searchCityApi(inputCity.value); //9. the value is called by a new function that uses the API *inputCity.value is redefined in the searchCityApi function parameter
 }
 
-function showForecast(){ //FORE 2. this function prevents us from repeating 5 times in HTML
-    let days = ["Mon", "Tue", "Wed", "Thu", "Fri"]; //FORE 3. array with days for the loop
-    let foreHtml = ""; //FORE 4. HTML empty first
+function getForeData(city){
+    apiKey = "28fac95a24b3ba61410a05dt43ob3b30";
+    apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(showForecast);
+}
 
-    days.forEach(function(day) { //FORE 5. loop repeats for each day, and insert in DAY 
-       foreHtml = foreHtml+ //FORE 6. empty HTML now added string and day
-        `<div class="forecast-info">
-            <div class="forecast-day">${day}</div>
-            <div class="forecast-icon">🌥️</div>
-            <div class="forecast-temperature"><span class="forecast-temp-bold">19°</span> <span class="forecast-temp">10°</span></div>
-        </div>`;
-    }); 
+function transformDate(time) {
+    const date = new Date(time * 1000);
+    const dayWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    return dayWeek[date.getDay()];
+}
+
+function showForecast(response){ //FORE 2. this function prevents us from repeating 5 times in HTML
+    
+    //let days = ["Mon", "Tue", "Wed", "Thu", "Fri"]; //FORE 3. array with days for the loop ||| Not necessary when using API
+    let foreHtml = ""; //FORE 4. HTML empty first
+    response.data.daily.forEach(function (info, index) {
+        if (index < 5) {
+            //days.forEach(function(day) { //FORE 5. loop repeats for each day, and insert in DAY 
+            foreHtml = foreHtml + //FORE 6. empty HTML now added string and day
+                `<div class="forecast-info">
+                    <div class="forecast-day">${transformDate(info.time)}</div>
+                    <img src="${info.condition.icon_url}" class="forecast-icon">
+                    <div class="forecast-temperature"><span class="forecast-temp-bold">${Math.round(info.temperature.maximum)}°</span> <span class="forecast-temp">${Math.round(info.temperature.minimum)}°</span></div>
+                </div>`;
+        }
+    });
+
+    let newForecast = document.querySelector("#forecast-section"); //FORE 7. select the section ID where I want my func
     newForecast.innerHTML = foreHtml //FORE 8. Add HTML in my selected DIV
 }
-    
-let newForecast = document.querySelector("#forecast-section"); //FORE 7. select the section ID where I want my func
-    
+
 let searchCity = document.querySelector("#search-city"); //1. Select the form, and submit button
 searchCity.addEventListener("submit", showCity) //2. name of function (1)
 
